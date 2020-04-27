2e2daa9421ca0e334579e7f61dc618c2cfd63ce5
jona506a <jobu@live.dk>
2020-04-27 10:23:59 +0200

    auto-commit via gh-pages

---
+++ myApp/precache-manifest.33630c004b99fbfc115a97ade223677a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fcb26f640270c3b61a30ecc2eddf2c",
+    "url": "/myApp/index.html"
+  },
+  {
+    "revision": "6a7bd34cb5229197ca2a",
+    "url": "/myApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16d2a3b5ab6d21986728",
+    "url": "/myApp/static/js/2.8d5b8e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myApp/static/js/2.8d5b8e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7bd34cb5229197ca2a",
+    "url": "/myApp/static/js/main.cddcff58.chunk.js"
+  },
+  {
+    "revision": "1b81c3a650d54eed4c99",
+    "url": "/myApp/static/js/runtime-main.f32175a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
